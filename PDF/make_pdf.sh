b44ae0c2bfc2c29cdb8641ed09c0b3f17f6aee04
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2016-06-03 11:21:46 -0300

    ajuste da contra-capa

--- PDF/make_pdf.sh
+++ PDF/make_pdf.sh
@@ -2,11 +2,11 @@ echo "Generating lowres pages:"
 for f in ../originais/*.tiff
 do
     echo "Converting $f ..."
-    convert $f -resize 2000 $f.lowres.jpg
+    convert $f -resize 1200 $f.lowres.jpg
 done
 
 echo "And now generating the lowres PDF..."
-convert ../originais/*.lowres.jpg Projeto_Logico_da_Unidade_de_Controle_de_um_Minicomputador__EFregni_1972.pdf
+convert ../originais/capa*.lowres.jpg ../originais/pagina*.lowres.jpg ../originais/pranchas*.lowres.jpg Projeto_Logico_da_Unidade_de_Controle_de_um_Minicomputador__EFregni_1972.pdf
 rm ../originais/*.lowres.jpg -f
 
 echo "done."
